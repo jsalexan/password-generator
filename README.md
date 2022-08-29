fda017e77fa7e2aa02d8ac5d23d3603c05bebc1f
Platypus <jennifersalexander@gmail.com>
2022-08-29 16:26:15 -0700

    Added HTML, CSS, and JS

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # password-generator
 We're creating a random password generator.
+
+
+
+
+## Links
+
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Addition_assignment
+
+https://www.w3schools.com/js/js_functions.asp
+
+https://www.w3schools.com/js/js_random.asp
+
+https://www.w3schools.com/jsref/jsref_parseInt.asp
+
+https://stackoverflow.com/questions/27037096/how-to-specify-range-on-math-random
+
+https://stackoverflow.com/questions/39766002/how-to-unset-variable-using-javascript
--- script.js
+++ script.js
@@ -1,29 +1,76 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Variable that will hold the user's choice of characters.
+var useableCharacters = "";
+
+// Variables for the various character-type choices.
+var alphaLower = "abcdefghijklmnopqrstuvwxyz";
+var alphaUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numbers = "0123456789";
+var symbols = "!@#$%^&*-_=+*";
+
+//Main function that will determine all the elements that go into the password.
 function generatePassword() {
-  console.log("Your password is generating.");
+  
+// Pop up prompt for user to select the length of their password.
+  var userCharNum = window.prompt("Choose a password length between 8 and 128 characters.")
 
-  var alphaLower = "abcdefghijklmnopqrstuvwxyz"
-  var alphaUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  var numbers = "0123456789";
-  var symbols = "!@#$%^&*_-+=";
+  // An integer will be parsed from a string.
+  var numCharacters = parseInt(userCharNum)
 
-prompt 
-//Prompt to select criteria: 
-  // - How many characters? 8 - 128 (prompt)
-  //
-  // - Include lowercase characters? (prompt with boolean)
-  // - Include uppercase characters? (prompt with boolean)
-  // - Include numbers? (prompt with boolean)
-  // - Include special characters? (prompt with boolean)
-  //
-  // - Ensure at lease one value type has been selected and that a character length is picked. (input validation) If not, an error message.(code)
-  // - Math code for random generation (code)
+// If the user types in characters that are not numbers, they will get thie alert.
+  if (isNaN(numCharacters)) {
+    window.alert("Please start over and enter a valid number between 8-128.")
+    return
+  } 
+// If the user types in numbers outside the given parameters, they will get thie alert.
+  if (numCharacters < 8 || numCharacters > 128) {
+    window.alert("Please start over and enter a valid number between 8-128.")
+    return
+  }
+  // TODO: Do I want to use this? else {var choice1 = window.prompt ("Do you want to include lowercase letters?")}
 
-  return ("GENERATED PASSWORD");
+//The following four varients will be true or false based on the user response to the pop up questions.
+var userAlphaLower = window.confirm("Do you want to include lowercase letters? OK for yes, Cancel for no.")
+
+var userAlphaUpper = window.confirm("Do you want to include uppercase letters? OK for yes, Cancel for no.")
+
+var userNumbers = window.confirm("Do you want to include numbers? OK for yes, Cancel for no.")
+
+var userSymbols = window.confirm("Do you want to include symbols? OK for yes, Cancel for no.")
+
+if (
+  userAlphaUpper === false &&
+  userAlphaLower === false &&
+  userNumbers === false &&
+  userSymbols === false
+) {
+  window.alert("You must select at least one character type. Please start over.");
+  generatePassword();
 }
 
+if (userAlphaUpper===true) {
+  useableCharacters = useableCharacters.concat(alphaUpper)
+}
+if (userAlphaLower===true) {
+  useableCharacters = useableCharacters.concat(alphaLower)
+}
+if (userNumbers===true) {
+  useableCharacters = useableCharacters.concat(numbers)
+}
+if (userSymbols===true) {
+  useableCharacters = useableCharacters.concat(symbols)
+}
+console.log(useableCharacters)
+
+var password = "";
+
+for (var i = 0; i < numCharacters; i++) {
+  password += useableCharacters.charAt(Math.floor(Math.random() * useableCharacters.length))
+}
+return password
+}
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -35,3 +82,20 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+
+
+
+//Prompt to select criteria: 
+  // - How many characters? 8 - 128 (prompt)
+  //
+  // - Include lowercase characters? (prompt with boolean)
+  // - Include uppercase characters? (prompt with boolean)
+  // - Include numbers? (prompt with boolean)
+  // - Include special characters? (prompt with boolean)
+  //
+  // - Ensure at lease one value type has been selected and that a character length is picked. (input validation) If not, an error message.(code)
+  // - Math code for random generation (code)
+
+  // return ("GENERATED PASSWORD");
