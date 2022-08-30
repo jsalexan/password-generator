fae14f3a9d1dae1ef457d1401726851294e73d98
Platypus <jennifersalexander@gmail.com>
2022-08-29 17:45:26 -0700

    Some changes, but I think they need to be overwritten

--- script.js
+++ script.js
@@ -2,13 +2,17 @@
 var generateBtn = document.querySelector("#generate");
 
 // Variable that will hold the user's choice of characters.
-var useableCharacters = "";
+var useableCharacters = [];
+var userAlphaUpper = undefined;
+var userAlphaLower = undefined;
+var userNumbers = undefined;
+var userSymbols = undefined;
 
 // Variables for the various character-type choices.
-var alphaLower = "abcdefghijklmnopqrstuvwxyz";
-var alphaUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var numbers = "0123456789";
-var symbols = "!@#$%^&*-_=+*";
+var alphaLower = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var alphaUpper = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+var symbols = ['!', '@', '#', '$', '%', '^', '&', '*', '-', '_', '=', '+', '*']; 
 
 //Main function that will determine all the elements that go into the password.
 function generatePassword() {
@@ -29,7 +33,7 @@ function generatePassword() {
     window.alert("Please start over and enter a valid number between 8-128.")
     return
   }
-  // TODO: Do I want to use this? else {var choice1 = window.prompt ("Do you want to include lowercase letters?")}
+  // TODO: Do I want to use this? else {var userAlphaLower = window.prompt ("Do you want to include lowercase letters?")}
 
 //The following four varients will be true or false based on the user response to the pop up questions.
 var userAlphaLower = window.confirm("Do you want to include lowercase letters? OK for yes, Cancel for no.")
@@ -45,9 +49,10 @@ if (
   userAlphaLower === false &&
   userNumbers === false &&
   userSymbols === false
-) {
+) 
+{
   window.alert("You must select at least one character type. Please start over.");
-  generatePassword();
+ 
 }
 
 if (userAlphaUpper===true) {
@@ -62,7 +67,10 @@ if (userNumbers===true) {
 if (userSymbols===true) {
   useableCharacters = useableCharacters.concat(symbols)
 }
+
 console.log(useableCharacters)
+
+generatePassword();
 
 var password = "";
 
