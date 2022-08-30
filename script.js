62eeb1d5896929e28ca1cadd240b2c747138a51d
Platypus <jennifersalexander@gmail.com>
2022-08-29 18:29:57 -0700

    Updated code and added comments

--- script.js
+++ script.js
@@ -1,21 +1,17 @@
 // Assignment Code
+//Main function that will determine all the elements that go into the password.
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword() {
+
 // Variable that will hold the user's choice of characters.
-var useableCharacters = [];
-var userAlphaUpper = undefined;
-var userAlphaLower = undefined;
-var userNumbers = undefined;
-var userSymbols = undefined;
+var useableCharacters = "";
 
 // Variables for the various character-type choices.
-var alphaLower = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-var alphaUpper = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-var symbols = ['!', '@', '#', '$', '%', '^', '&', '*', '-', '_', '=', '+', '*']; 
-
-//Main function that will determine all the elements that go into the password.
-function generatePassword() {
+var alphaLower = "abcdefghijklmnopqrstuvwxyz";
+var alphaUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numbers = "0123456789";
+var symbols = "!@#$%^&*-_=+*";
   
 // Pop up prompt for user to select the length of their password.
   var userCharNum = window.prompt("Choose a password length between 8 and 128 characters.")
@@ -33,27 +29,25 @@ function generatePassword() {
     window.alert("Please start over and enter a valid number between 8-128.")
     return
   }
-  // TODO: Do I want to use this? else {var userAlphaLower = window.prompt ("Do you want to include lowercase letters?")}
+  // TODO: Do I want to use an else?
 
 //The following four varients will be true or false based on the user response to the pop up questions.
-var userAlphaLower = window.confirm("Do you want to include lowercase letters? OK for yes, Cancel for no.")
+var userAlphaLower = window.confirm("Do you want to include lowercase letters? OK for yes, Cancel for no.");
 
-var userAlphaUpper = window.confirm("Do you want to include uppercase letters? OK for yes, Cancel for no.")
+var userAlphaUpper = window.confirm("Do you want to include uppercase letters? OK for yes, Cancel for no.");
 
-var userNumbers = window.confirm("Do you want to include numbers? OK for yes, Cancel for no.")
+var userNumbers = window.confirm("Do you want to include numbers? OK for yes, Cancel for no.");
 
-var userSymbols = window.confirm("Do you want to include symbols? OK for yes, Cancel for no.")
+var userSymbols = window.confirm("Do you want to include symbols? OK for yes, Cancel for no.");
 
 if (
   userAlphaUpper === false &&
   userAlphaLower === false &&
   userNumbers === false &&
   userSymbols === false
-) 
-{
+) {
   window.alert("You must select at least one character type. Please start over.");
- 
-}
+  }
 
 if (userAlphaUpper===true) {
   useableCharacters = useableCharacters.concat(alphaUpper)
@@ -67,10 +61,7 @@ if (userNumbers===true) {
 if (userSymbols===true) {
   useableCharacters = useableCharacters.concat(symbols)
 }
-
 console.log(useableCharacters)
-
-generatePassword();
 
 var password = "";
 
@@ -85,7 +76,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
